70d36577850fc96ecc6fbe38cbd6c354e6cec0ec
J. Francisco Martín <jfm.lisaso@gmx.com>
2024-02-08 11:35:46 +0100

    feat: basic ink connection
    It allows to traverse the ink story tree.

--- src/components/engine/StoryProvider/StoryProvider.tsx
+++ src/components/engine/StoryProvider/StoryProvider.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "@mui/material/Button";
 import { styled, useTheme } from "@mui/material/styles";
+import { Story } from "inkjs";
 import AppBar from "../AppBar";
 import Container from "../Container";
 
@@ -8,6 +10,47 @@ export const StoryContext = React.createContext({});
 export const StoryProvider = () => {
   const theme = useTheme();
 
+  const [story, setStory] = React.useState<InstanceType<typeof Story>>();
+  const [contents, setContents] = React.useState<(string | null)[]>([]);
+
+  React.useEffect(() => {
+    const fetchStory = async () => {
+      const storyData = await fetch("ulises.json");
+      const result = await storyData.text();
+      setStory(new Story(result));
+    };
+
+    fetchStory();
+  }, []);
+
+  const handleContinue = () => {
+    if (story?.canContinue) {
+      const updatedContents: (string | null)[] = [...contents];
+      updatedContents.push(story.Continue());
+      setContents(updatedContents);
+    }
+  };
+
+  const printOptions = () => {
+    return story?.canContinue ? (
+      <Button onClick={handleContinue}>Continuar</Button>
+    ) : (
+      <>
+        {story?.currentChoices.map((element, index) => (
+          <Button
+            key={`option-${index}`}
+            onClick={() => {
+              story.ChooseChoiceIndex(index);
+              handleContinue();
+            }}
+          >
+            {element.text}
+          </Button>
+        ))}
+      </>
+    );
+  };
+
   return (
     <StoryContext.Provider value={{}}>
       <Main>
@@ -15,7 +58,12 @@ export const StoryProvider = () => {
           title="Ulises"
           TitleProps={{ timeout: theme.transitions.duration.standard }}
         />
-        <Container maxWidth="sm">Contents...</Container>
+        <Container maxWidth="sm">
+          {contents.map((element, index) => (
+            <p key={`content-${index}`}>{element}</p>
+          ))}
+          {printOptions()}
+        </Container>
       </Main>
     </StoryContext.Provider>
   );
